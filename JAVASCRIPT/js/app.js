d23fa5cd93ca3c6692a0df0d90171ebd10708b1c
eldiego77cp <88398189+eldiego77cp@users.noreply.github.com>
2021-09-23 01:26:07 -0300

    Upd 23092021 1:25

---
+++ JAVASCRIPT/js/app.js
@@ -0,0 +1,43 @@
+// (1) alert("Hello World");
+
+// (2) document.write("Hello World");
+
+// (3) document.write(3+5);
+
+/* (4) let nombreUsuario = prompt("Ingrese el nombre de usuario ", "")
+document.write(`Hola ${nombreUsuario}`)*/
+
+/* (5)  let nro1 = prompt("Ingrese un número ", "")
+        let nro2 = prompt("Ingrese otro número ", "")
+        document.write(Number.parseInt(nro1) + Number.parseInt(nro2))*/
+
+/* (6)      let nro1 = prompt("Ingrese un número ", "");
+            let nro2 = prompt("Ingrese otro número ", "");
+            if (Number.parseInt(nro1) > Number.parseInt(nro2)) {
+                document.write(`El mayor número de ambos es ${nro1}`);
+            }
+            else {
+                    document.write(`El mayor número de ambos es ${nro2}`);
+            } */
+
+/*(7)       let nro1 = prompt("Ingrese un número ", "");
+            let nro2 = prompt("Ingrese otro número ", "");
+            let nro3 = prompt("Ingrese otro número ", "");
+            if (Number.parseInt(nro1) > Number.parseInt(nro2) && Number.parseInt(nro1) > Number.parseInt(nro3)) {
+                document.write(`El mayor número de ambos es ${nro1}`);
+            }
+            else if (Number.parseInt(nro2) > Number.parseInt(nro1) && Number.parseInt(nro2) > Number.parseInt(nro3)) {
+                    document.write(`El mayor número de ambos es ${nro2}`);
+            }
+            else if (Number.parseInt(nro3) > Number.parseInt(nro1) && Number.parseInt(nro3) > Number.parseInt(nro2)) {
+                document.write(`El mayor número de ambos es ${nro3}`);
+            }*/
+
+/*(8)       let nro1 = prompt("Ingrese un número ", "");
+            if (Number.parseInt(nro1) % 2 == 0) {
+            document.write(`El número ${nro1} es divisible por 2`);
+            }*/
+
+/*(9)*/     let frase = prompt("Ingrese una frase ", "");
+            
+            
