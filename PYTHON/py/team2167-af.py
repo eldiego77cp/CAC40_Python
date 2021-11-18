015c731cbc12bb3ed65602006233bbee1ff42b5f
eldiego77cp <88398189+eldiego77cp@users.noreply.github.com>
2021-11-18 02:01:49 -0300

    Upd 18112021 2:01

---
+++ PYTHON/py/team2167-af.py
@@ -0,0 +1,26 @@
+def replaceChar (string, index, character):
+    '''
+    Reemplaza un caracter del string ingresado basado en el índice de la posición a reemplazar y el nuevo caracter
+
+    Parámetros
+    
+    string: str
+    index: int
+    character: str
+    
+    '''
+    newList = list(string)
+    
+    if index > len(newList):
+        return print("El indice supera a la cantidad de caracteres del string ingresado")
+    elif len(character) > 1:
+        return print("La cantidad de caracteres a reemplazar tiene que ser 1")
+    else:
+        newList[index] = character
+    
+    newString = "".join(newList)
+    
+    return print(newString)
+
+
+
